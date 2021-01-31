a05cbc729563eafc97283a430327845cd652704b
zinirun <heojj97@naver.com>
2021-01-31 22:34:09 +0900

    Init graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GraphQLModule.forRoot({})],
 })
 export class AppModule {}
