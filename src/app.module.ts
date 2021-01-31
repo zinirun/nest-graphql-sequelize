7bed84d56ad9c95d0934c89ff5f35e6b8f7a3164
zinirun <heojj97@naver.com>
2021-01-31 22:35:18 +0900

    Set eslint, prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+    imports: [GraphQLModule.forRoot({})],
 })
 export class AppModule {}
