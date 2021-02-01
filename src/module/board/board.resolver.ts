d82bc4134e1cee7b1a4bb76212616ebddf0ce3fd
zinirun <heojj97@naver.com>
2021-02-01 23:25:37 +0900

    Add board graphql modules

--- src/module/board/board.resolver.ts
+++ src/module/board/board.resolver.ts
@@ -1,4 +1,33 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Post, PostInput } from 'src/autogen/schema.graphql';
+import { BoardService } from './board.service';
 
 @Resolver()
-export class BoardResolver {}
+export class BoardResolver {
+    constructor(private readonly boardService: BoardService) {}
+
+    @Query('getPosts')
+    async getAll(): Promise<Post[]> {
+        return await this.boardService.getAll();
+    }
+
+    @Query('getPostById')
+    async getPostById(@Args('id') id: number): Promise<Post> {
+        return await this.boardService.getOne(id);
+    }
+
+    @Mutation('createPost')
+    async createPost(@Args('userId') userId: string, @Args('post') post: PostInput): Promise<Post> {
+        return await this.boardService.create(userId, post);
+    }
+
+    @Mutation('updatePost')
+    async updatePost(@Args('id') id: number, @Args('Post') post: PostInput): Promise<Post> {
+        return await this.boardService.update(id, post);
+    }
+
+    @Mutation('deletePost')
+    async deletePost(@Args('id') id: number): Promise<number> {
+        return await this.boardService.delete(id);
+    }
+}
--- src/module/board/board.service.ts
+++ src/module/board/board.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Post, PostInput } from 'src/autogen/schema.graphql';
+import moment from 'moment';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+    private posts: Post[] = [];
+
+    async getAll(): Promise<Post[]> {
+        return this.posts;
+    }
+
+    async getOne(id: number): Promise<Post> {
+        const post = this.posts.find((post) => post.id === id);
+        if (!post) {
+            throw new NotFoundException(`Post with ID ${id}: Not Found`);
+        }
+        return post;
+    }
+
+    async create(userId: string, post: PostInput): Promise<Post> {
+        const id = this.posts.length + 1;
+        this.posts.push({
+            id,
+            userId,
+            createdAt: moment().toDate(),
+            ...post,
+        });
+        return await this.getOne(id);
+    }
+
+    async update(id: number, post: PostInput): Promise<Post> {
+        await this.getOne(id);
+        this.posts = this.posts.map((p) => (p.id === id ? { ...p, ...post } : p));
+        return await this.getOne(id);
+    }
+
+    async delete(id: number): Promise<number> {
+        await this.getOne(id);
+        this.posts = this.posts.filter((post) => post.id !== id);
+        return id;
+    }
+}
