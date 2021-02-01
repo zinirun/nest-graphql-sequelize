2d00f8f4052794e051f3a8906c3a958ca920274b
zinirun <heojj97@naver.com>
2021-02-01 23:37:51 +0900

    Add unit test - board

--- src/module/board/board.service.spec.ts
+++ src/module/board/board.service.spec.ts
@@ -1,18 +1,97 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardService } from './board.service';
 
 describe('BoardService', () => {
-  let service: BoardService;
+    let service: BoardService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BoardService],
-    }).compile();
+    beforeAll(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [BoardService],
+        }).compile();
 
-    service = module.get<BoardService>(BoardService);
-  });
+        service = module.get<BoardService>(BoardService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe('getAll', () => {
+        it('should return all posts', async () => {
+            const result = await service.getAll();
+            expect(result).toBeInstanceOf(Array);
+        });
+    });
+
+    // Add Sample Post and use for other tests
+    describe('create', () => {
+        it('should create', async () => {
+            const beforeCreate = await service.getAll().then((posts) => posts.length);
+            const samplePost = {
+                title: 'sample-title',
+                content: 'sample-content',
+            };
+            await service.create('test-userId', samplePost);
+            const afterCreate = await service.getAll().then((posts) => posts.length);
+            expect(afterCreate).toBeGreaterThan(beforeCreate);
+        });
+    });
+
+    describe('getOne', () => {
+        it('should get one post', async () => {
+            const post = await service
+                .getOne(1)
+                .then((post) => post)
+                .catch((err) => err);
+            expect(post.id).toBe(1);
+        });
+        it('should throw NotFoundException', async () => {
+            const post = await service
+                .getOne(999)
+                .then((post) => post)
+                .catch((err) => err);
+            expect(post).toBeInstanceOf(NotFoundException);
+        });
+    });
+
+    describe('update', () => {
+        it('should update post', async () => {
+            const post = await service
+                .update(1, {
+                    title: 'update-title',
+                    content: 'update-content',
+                })
+                .then((post) => post)
+                .catch((err) => err);
+            expect(post.title).toBe('update-title');
+        });
+        it('should throw NotFoundException', async () => {
+            const post = await service
+                .update(999, {
+                    title: 'update-title',
+                    content: 'update-content',
+                })
+                .then((post) => post)
+                .catch((err) => err);
+            expect(post).toBeInstanceOf(NotFoundException);
+        });
+    });
+
+    describe('delete', () => {
+        it('should delete post', async () => {
+            const id = await service
+                .delete(1)
+                .then((id) => id)
+                .catch((err) => err);
+            expect(id).toBe(1);
+        });
+        it('should throw NotFoundException', async () => {
+            const idUnvalid = await service
+                .delete(999)
+                .then((id) => id)
+                .catch((err) => err);
+            expect(idUnvalid).toBeInstanceOf(NotFoundException);
+        });
+    });
 });
